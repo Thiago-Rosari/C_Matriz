002ed056c71b622e8183f584068723f2623c59eb
macielthiago <maciel.rosari54@gmail.com>
2026-01-09 18:53:30 -0400

    Nova instrução

--- Aulas/aula001/olaMundo.c
+++ Aulas/aula001/olaMundo.c
@@ -6,6 +6,7 @@ int main(void) {
     // Comentários de uma linha iniciam com // (duas barras).
     // A função printf direriona o texto inserido (entre aspas duplas) para saída ádrão.
     printf("Ola, mundo!!");
+    printf("Curso de C");
 
     return 0;
 }
